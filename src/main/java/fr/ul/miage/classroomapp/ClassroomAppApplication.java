ec909d2f39998007694739bd15273f05ee0ef9cd
jerembci <49069337+jerembci@users.noreply.github.com>
2022-01-27 10:00:03 +0100

    Project initialization

---
+++ src/main/java/fr/ul/miage/classroomapp/ClassroomAppApplication.java
@@ -0,0 +1,13 @@
+package fr.ul.miage.classroomapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroomAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ul/miage/classroomapp/ClassroomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ul.miage.classroomapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
