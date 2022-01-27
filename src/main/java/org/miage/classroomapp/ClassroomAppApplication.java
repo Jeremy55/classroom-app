9091f303ee119ce5916120260fed53d01483e556
Jérémy Picard <jeremy55200@hotmail.com>
2022-01-27 09:47:33 +0100

    Initialized empty springboot app.

---
+++ src/main/java/org/miage/classroomapp/ClassroomAppApplication.java
@@ -0,0 +1,13 @@
+package org.miage.classroomapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassroomAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/miage/classroomapp/ClassroomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.miage.classroomapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
