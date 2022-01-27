4193ffc7aed4f81d1b3401bf81fa0665ab91cda0
Elouan <elouanbrst@gmail.com>
2022-01-27 10:02:36 +0100

    cruds for course, student, teacher

---
+++ app/src/Form/TeacherType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Teacher;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TeacherType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Lastname')
+            ->add('Firstname')
+            ->add('Subject')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Teacher::class,
+        ]);
+    }
+}
