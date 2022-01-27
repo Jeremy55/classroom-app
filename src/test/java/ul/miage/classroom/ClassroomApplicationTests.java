316af077996ace463bab551f4b442dbff7b283c0
Chloé <chloe.soquet@hotmail.com>
2022-01-27 10:00:04 +0100

    initial commit

---
+++ src/test/java/ul/miage/classroom/ClassroomApplicationTests.java
@@ -0,0 +1,13 @@
+package ul.miage.classroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
