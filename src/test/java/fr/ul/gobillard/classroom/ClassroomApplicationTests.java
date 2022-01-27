2faa0f5e870e1d165ee24782d9e8865f8bc37001
tom gobillard <tom.goo@hotmil.com>
2022-01-27 10:02:11 +0100

    first commit

---
+++ src/test/java/fr/ul/gobillard/classroom/ClassroomApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ul.gobillard.classroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
