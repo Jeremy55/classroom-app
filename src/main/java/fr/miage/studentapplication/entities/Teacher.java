8a7c5fc8b57981299b9363f4eb260c280821ef16
Maxime Marigliano <maxim.marigliano@gmail.com>
2022-01-27 10:12:27 +0100

    entities

---
+++ src/main/java/fr/miage/studentapplication/entities/Teacher.java
@@ -0,0 +1,17 @@
+package fr.miage.studentapplication.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+public class Teacher {
+    private int id;
+
+    private String name;
+    private String firstName;
+}
