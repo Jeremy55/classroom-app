6619a87b9d8570cc4c984c30a0bad43314b711dc
Vellucen <gabriel.couroux33@orange.fr>
2022-01-27 11:49:39 +0100

    Crate maven project and SpringBoot app

---
+++ src/main/java/fr/ul/miage/classroom_app/ClassroomAppServiceApplication.java
@@ -0,0 +1,11 @@
+package fr.ul.miage.classroom_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomAppServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClassroomAppServiceApplication.class, args);
+    }
+}
